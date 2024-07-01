5b8d5c6e2c5948fa42408b46265f04e521f5225b
FreeSoulCris <c.jimenez@witbooking.com>
2024-07-02 00:24:28 +0200

    Unit 4: I created the Dictionary component and added a form. I also added the footer.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import logo from "./logo.png"
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" ></img>
       </header>
-      <body>
-        <div>
-        <p>Dictionary</p>
-        </div>
-        
-      </body>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="App-footer">
+          This project was coded by {" "}<a href="https://www.instagram.com/freesoulcriss/" target="_blank" rel=" noopener noreferrer">Cristina Jimenez</a>, is open-sourced on {" "}
+          <a href="https://github.com/FreeSoulCris/react-dictionary-project" target="_blank" rel="noopener noreferrer">Github </a> and hosted on {" "}<a href="https://curious-dictionary-app.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>
+       </footer>
+      </div>
     </div>
   );
 }
