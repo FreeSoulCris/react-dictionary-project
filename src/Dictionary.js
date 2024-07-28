65ba8d4385cac83669515aab279e3d4773e2f13f
FreeSoulCris <c.jimenez@witbooking.com>
2024-07-28 20:51:56 +0200

    I added the Photo Gallery Component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,23 +2,35 @@ import React, {useState} from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 
 export default function Dictionary (props){
     let [keyword, setKeyword] = useState (props.defaultKeyword);
     let [results, setResults ] = useState(null);
     let [loaded, setLoaded] = useState (false);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response){
+    function handleDictionaryResponse(response){
         setResults(response.data);
+    }
 
+    function handlePexelsResponse (response){
+       console.log(response);
+       setPhotos(response.data.photos);
     }
 
     function search(){
-        let key ="0239330ab540e803o5b4f9t7e63fbef4" 
-        let apiUrl =`https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`
+        let key ="0239330ab540e803o5b4f9t7e63fbef4"; 
+        let apiUrl =`https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
   
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+
+        let pexelsApiKey ="YcMDB4oAuY4SBcUM16wmmy9Fd9USmCqZ3K3dd2rTZpTk5Wni7LjYpjBV";
+        let pexelsApiUrl =`https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = { Authorization: `${pexelsApiKey}`};
+
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function handleSubmit(event){
@@ -52,6 +64,7 @@ export default function Dictionary (props){
                 </div>
             </section>   
             <Results results={results}/>
+            <Photos photos={photos}/>
        </div>
         
     );
